260939a4a9bc2c5677476c3e0a747bb23c70611d
Johan Hagenbjörk <johan.hagenbjork@gmail.com>
2023-06-28 07:09:21 +0200

    Added Receiver and setup callbacks for incoming pump messages. Removed pump cooldown due to timer limitations.

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import ubinascii
 from machine import Pin, Timer  # There are 15 timers available, use carefully!
 from functools import partial
 from umqtt.simple import MQTTClient
-from password import wifi, hivemq
+from mqtt import Receiver
+from password import wifi, hivemq, sensor_config
 from sensor import MoistureSensor
 from observable import ObservableValue, ObservableSum
 from pump import Pump
@@ -18,21 +19,21 @@ Timer(-1).init(period=TWENTYFOUR_HOURS_IN_MILLISECONDS, mode=Timer.ONE_SHOT, cal
 
 pump_config = {
     'pump_duration': 5_000,
-    'pump_cooldown': 10_000,
-    'pump_cap_time': 1_000 * 60 * 10
+    'pump_cooldown': 0,
+    'pump_cap_time': 1_000 * 200
 }
-sensor_config = {
-    'min_humidity': 0.0,
-    'max_humidity': 60.0,
+humidity_config = {
+    'min_humidity': 30.0,
+    'max_humidity': 75.0,
     'sensor_cooldown_period_watering': 1_000 * 15,
-    'sensor_cooldown_period_draining': 1_000 * 15 * 60
+    'sensor_cooldown_period_draining': 1_000 * 5 * 60
 }
 
 buzz = Pin(15, Pin.OUT)
-pumps = [Pump(Pin(pin, Pin.OUT), name=f'Pump{pin}') for pin in range(2, 6)]
+pumps = [Pump(Pin(pin, Pin.OUT), name=f'Pump') for pin in range(2, 6)]
 leds = [Pin(pin, Pin.OUT, value=0) for pin in range(7, 11)]
 buttons = [Pin(pin, Pin.IN, machine.Pin.PULL_UP) for pin in range(11, 15)]
-sensors = [MoistureSensor(Pin(pin), name='Moisture', period=1_000) for pin in range(26, 29)]
+sensors = [MoistureSensor(Pin(pin), name='Moisture', period=1_000, **sensor_config) for pin in range(26, 29)]
 
 
 # Manual pump control
@@ -66,12 +67,13 @@ client = MQTTClient(
     port=0,
     user=hivemq.get('user'),
     password=hivemq.get('password'),
-    keepalive=7200,
+    keepalive=600,
     ssl=True,
     ssl_params={'server_hostname': hivemq.get('server')}
 )
+receiver = Receiver(client)
 
-for i in range(100):
+for i in range(10):
     utime.sleep(1)
     print('Waiting for connection...')
     if wlan.isconnected():
@@ -84,15 +86,19 @@ else:
     print(f'Connected on {ip}')
     status = client.connect()
     print(f'MQTT client connected with status {status}')
-    client = None
-    
 
 # Irrigation setup
 irrigations = []
-irrigations.append(Irrigation('Blueberry', sensors[0], pumps[0], client, **pump_config, **sensor_config, pump_cap_pin=leds[0]))
-irrigations.append(Irrigation('Smultron', sensors[1], pumps[1], client, **pump_config, **sensor_config, pump_cap_pin=leds[1]))
-irrigations.append(Irrigation('Tomato', sensors[2], pumps[2], client, **pump_config, **sensor_config, pump_cap_pin=leds[2]))
-irrigations.append(Irrigation('Zinnia', sensors[2], pumps[3], client, **pump_config, **sensor_config, pump_cap_pin=leds[3]))
+irrigations.append(Irrigation('Blueberry', sensors[0], pumps[0], client, **pump_config, **humidity_config, pump_cap_pin=leds[0]))
+irrigations.append(Irrigation('Smultron', sensors[1], pumps[1], client, **pump_config, **humidity_config, pump_cap_pin=leds[1]))
+irrigations.append(Irrigation('Cucumber', sensors[2], pumps[2], client, **pump_config, **humidity_config, pump_cap_pin=leds[2]))
+irrigations.append(Irrigation('Zinnia', sensors[2], pumps[3], client, **pump_config, **humidity_config, pump_cap_pin=leds[3]))
+
+for irr in irrigations:
+    topic = bytes(f'{irr.name()}/Pump', 'utf-8')
+    receiver.subscribe(topic, lambda msg, irr=irr: irr._start_pump(int(msg.decode('utf-8'))))  # Force capture of irr
+
 
 while True:
     machine.idle()
+    client.check_msg()
